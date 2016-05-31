18f69f5f974c16a3379c879adc48d5e83fbf8bb3
Erin Millard <ezzatron@gmail.com>
2016-05-31 17:00:07 +1000

    PHP fixes.

--- setup/2-php.bash
+++ setup/2-php.bash
@@ -1,16 +1,17 @@
 echo "Installing PHP..."
 
 brew install \
-    homebrew/php/php53 --with-phpdbg \
-    homebrew/php/php53-intl
+    homebrew/php/php53 \
+    homebrew/php/php53-intl \
     homebrew/php/php54 --with-phpdbg \
-    homebrew/php/php54-intl
+    homebrew/php/php54-intl \
     homebrew/php/php55 --with-phpdbg \
-    homebrew/php/php55-intl
+    homebrew/php/php55-intl \
     homebrew/php/php56 --with-phpdbg \
-    homebrew/php/php56-intl
+    homebrew/php/php56-intl \
     homebrew/php/php70 --with-phpdbg \
-    homebrew/php/php70-intl
+    homebrew/php/php70-intl \
+    homebrew/php/composer
 
 PHP_ETC_PATH="$(brew --prefix)/etc/php"
 
@@ -36,13 +37,3 @@ popd > /dev/null
 pushd "$PHP_ETC_PATH/7.0" > /dev/null
 ln -s ../php.ini
 popd > /dev/null
-
-if [[ -f "$HOME/bin/composer" ]]; then
-    echo "Composer already installed."
-else
-    echo "Installing Composer..."
-    curl -fsSL "https://getcomposer.org/download/1.0.0/composer.phar" > "$HOME/bin/composer"
-fi
-
-echo "Updating Composer..."
-"$HOME/bin/composer" selfupdate

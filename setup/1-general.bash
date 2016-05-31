b5042baf232899f20741fbc3ec758d42ff82372e
Erin Millard <ezzatron@gmail.com>
2016-05-31 16:40:16 +1000

    Install pygments via gem.

--- setup/1-general.bash
+++ setup/1-general.bash
@@ -1,10 +1,11 @@
 [ -e "$HOME/bin" ] || mkdir -p "$HOME/bin"
 
+gem install --user-install \
+    pygments.rb \
+    travis
+
 brew install \
     bash-completion \
     ghi \
     git \
     hub
-
-gem install --user-install \
-    travis

794c078d1578f223c0aaa5c546c8581ae3506382
Erin Millard <ezzatron@gmail.com>
2016-06-01 20:19:32 +1000

    Automated Sublime Text license.

--- setup/3-apps.bash
+++ setup/3-apps.bash
@@ -12,3 +12,14 @@ brew cask install \
     spotify \
     steam \
     sublime-text3
+
+SUBLIME_CONFIG_PATH="$HOME/Library/Application Support/Sublime Text 3/Local"
+
+if [ -f "$SUBLIME_CONFIG_PATH/License.sublime_license" ]; then
+    echo "Sublime Text license already exists."
+else
+    echo "Fetching Sublime Text license from LastPass..."
+    mkdir -p "$SUBLIME_CONFIG_PATH"
+    lpass show "Personal/Sublime Text License" --notes \
+        > "$SUBLIME_CONFIG_PATH/License.sublime_license"
+fi

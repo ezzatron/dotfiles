f0442c11ef4faf115d3a2cf4d39e380166225afe
Erin Millard <ezzatron@gmail.com>
2016-06-01 15:52:56 +1000

    Fixing zsh setup.

--- setup/1-general.bash
+++ setup/1-general.bash
@@ -19,5 +19,13 @@ brew install \
     zsh-syntax-highlighting
 
 echo "Installing Oh My Zsh..."
+
+if grep -Fxq /usr/local/bin/zsh /etc/shells; then
+    echo "Brew zsh is already a standard shell."
+else
+    echo "Adding brew zsh as a standard shell..."
+    sudo sh -c 'echo /usr/local/bin/zsh > /etc/shells'
+fi
+
 chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

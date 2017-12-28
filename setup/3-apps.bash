9450464562ba0ad72afba27078b8c860007c5c0c
Erin Millard <ezzatron@gmail.com>
2017-12-28 15:58:04 +1000

    Moving Sublime license because of LastPass timeouts.

--- setup/3-apps.bash
+++ setup/3-apps.bash
@@ -15,17 +15,6 @@ brew cask install \
     steam \
     sublime-text
 
-SUBLIME_CONFIG_PATH="$HOME/Library/Application Support/Sublime Text 3/Local"
-
-if [ -f "$SUBLIME_CONFIG_PATH/License.sublime_license" ]; then
-    echo "Sublime Text license already exists."
-else
-    echo "Fetching Sublime Text license from LastPass..."
-    mkdir -p "$SUBLIME_CONFIG_PATH"
-    lpass show "Personal/Sublime Text License" --notes \
-        > "$SUBLIME_CONFIG_PATH/License.sublime_license"
-fi
-
 open "/Applications/Dropbox.app"
 open "/Applications/Google Chrome.app"
 open "/Applications/iTerm.app"

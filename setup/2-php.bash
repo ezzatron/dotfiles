3e7f718f46d4644c5a6472de2c8236b8e6352d7b
Erin Millard <ezzatron@gmail.com>
2018-10-15 11:12:34 +1000

    Update PHP installation

--- setup/2-php.bash
+++ setup/2-php.bash
@@ -3,19 +3,14 @@ set -e
 echo "Installing PHP..."
 
 brew tap ezzatron/brew-php
-brew tap homebrew/homebrew-php
 
 brew install brew-php
 
-brew php install php53 php53-intl
-brew php install php54 php54-intl
-brew php install php55 php55-intl
-brew php install php56 php56-intl
-brew php install php70 php70-intl
-brew php install php71 php71-intl
-brew php install php72 php72-intl
+brew php install php@7.0
+brew php install php@7.1
+brew php install php
 
-brew php link php72
+brew php link php
 
 brew php install composer
 
@@ -23,22 +18,6 @@ PHP_ETC_PATH="$(brew --prefix)/etc/php"
 
 echo "date.timezone = 'Australia/Brisbane'" > "$PHP_ETC_PATH/php.ini"
 echo "phar.readonly = 0" >> "$PHP_ETC_PATH/php.ini"
-
-pushd "$PHP_ETC_PATH/5.3" > /dev/null
-ln -fs ../php.ini
-popd > /dev/null
-
-pushd "$PHP_ETC_PATH/5.4" > /dev/null
-ln -fs ../php.ini
-popd > /dev/null
-
-pushd "$PHP_ETC_PATH/5.5" > /dev/null
-ln -fs ../php.ini
-popd > /dev/null
-
-pushd "$PHP_ETC_PATH/5.6" > /dev/null
-ln -fs ../php.ini
-popd > /dev/null
 
 pushd "$PHP_ETC_PATH/7.0" > /dev/null
 ln -fs ../php.ini

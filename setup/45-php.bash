81b39a51b23815d8cf5ff517b017433644c1ec2a
Erin Millard <ezzatron@gmail.com>
2019-12-02 08:59:37 +1000

    Improve PHP configuration

--- setup/45-php.bash
+++ setup/45-php.bash
@@ -2,10 +2,7 @@ set -e
 
 echo "Configuring PHP..."
 
-PHP_ETC_PATH="$(brew --prefix)/etc/php"
-echo "date.timezone = 'Australia/Brisbane'" > "$PHP_ETC_PATH/php.ini"
-echo "phar.readonly = 0" >> "$PHP_ETC_PATH/php.ini"
+PHP_INI_SCAN_DIR="$(php --ini | grep "conf\.d$" | sed "s/.*: \//\//")"
 
-pushd "$PHP_ETC_PATH/7.4" > /dev/null
-ln -fs ../php.ini
-popd > /dev/null
+echo "date.timezone = 'Australia/Brisbane'" > "$PHP_INI_SCAN_DIR/timezone.ini"
+echo "phar.readonly = 0" >> "$PHP_INI_SCAN_DIR/phar.ini"

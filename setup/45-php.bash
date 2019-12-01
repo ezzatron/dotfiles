14495019a3c5746383d4fd10043d8cec9778f191
Erin Millard <ezzatron@gmail.com>
2019-12-01 12:34:06 +1000

    Bump PHP versions

--- setup/45-php.bash
+++ setup/45-php.bash
@@ -6,6 +6,6 @@ PHP_ETC_PATH="$(brew --prefix)/etc/php"
 echo "date.timezone = 'Australia/Brisbane'" > "$PHP_ETC_PATH/php.ini"
 echo "phar.readonly = 0" >> "$PHP_ETC_PATH/php.ini"
 
-pushd "$PHP_ETC_PATH/7.3" > /dev/null
+pushd "$PHP_ETC_PATH/7.4" > /dev/null
 ln -fs ../php.ini
 popd > /dev/null

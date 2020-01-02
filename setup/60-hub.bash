feb421ee8b4fc1d8c2d347f34d6fd28c3d6eee1a
Erin Millard <ezzatron@gmail.com>
2020-01-03 09:43:17 +1000

    Configure Hub

---
+++ setup/60-hub.bash
@@ -0,0 +1,30 @@
+set -e
+
+echo "Configuring Hub..."
+
+if [[ -z "$GITHUB_USER" ]]; then
+    echo "GITHUB_USER not defined."
+    exit 1
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+    echo "GITHUB_TOKEN not defined."
+    exit 1
+fi
+
+mkdir -p "$HOME/.config"
+touch "$HOME/.config/hub"
+
+if grep -q github.com "$HOME/.config/hub"; then
+  echo "Hub is already configured for github.com"
+else
+  cat >> "$HOME/.config/hub" <<EOL
+github.com:
+- user: $GITHUB_USER
+  oauth_token: $GHE_TOKEN
+  protocol: https
+EOL
+  echo "Added Hub configuration for github.com"
+fi
+
+

03ec7979a67b7fdf5c11e591f957a1b433dbd018
Erin Millard <ezzatron@gmail.com>
2020-01-03 11:04:49 +1000

    Launch Docker

--- setup/90-open-apps.bash
+++ setup/90-open-apps.bash
@@ -4,6 +4,7 @@ echo "Opening apps..."
 
 open "/Applications/1Password 7.app"
 open "/Applications/Backup and Sync.app"
+open "/Applications/Docker.app"
 open "/Applications/Google Chrome.app"
 open "/Applications/MacMediaKeyForwarder.app"
 open "/Applications/Scroll Reverser.app"

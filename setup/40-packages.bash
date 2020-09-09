ef7d791f8387e87f44ab5fe406838a56202c3ce1
Erin Millard <ezzatron@gmail.com>
2020-09-10 09:39:00 +1000

    Remove Travis CLI

--- setup/40-packages.bash
+++ setup/40-packages.bash
@@ -2,9 +2,5 @@ set -e
 
 echo "Installing packages..."
 
-gem install --user-install \
-  pygments.rb \
-  travis
-
 brew bundle "--file=$HOME/Brewfile"
 brew link --force mysql-client
--- setup/65-ci.bash
+++
@@ -1,7 +0,0 @@
-set -e
-
-echo "Configuring Travis CLI..."
-
-TRAVIS_GH_TOKEN="$(op get item --account=my GitHub | jq --raw-output '.details.sections[]? | select(.title == "Tokens") | .fields[]? | select(.t == "Travis") | .v')"
-
-travis login --no-interactive --skip-completion-check --com --github-token "$TRAVIS_GH_TOKEN"

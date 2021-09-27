b0b40403ce3d4012c67fde7f5c290debbe01f61b
Erin Millard <ezzatron@gmail.com>
2021-09-27 17:56:28 +1000

    Replace Google B&S with Drive

--- setup/90-open-apps.bash
+++ setup/90-open-apps.bash
@@ -3,9 +3,9 @@ set -e
 echo "Opening apps..."
 
 open "/Applications/1Password 7.app"
-open "/Applications/Backup and Sync.app"
 open "/Applications/Docker.app"
 open "/Applications/Google Chrome.app"
+open "/Applications/Google Drive.app"
 open "/Applications/MacMediaKeyForwarder.app"
 open "/Applications/Scroll Reverser.app"
 open "/Applications/Slack.app"

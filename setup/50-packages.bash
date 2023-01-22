10e6a7e54827b7683455a14cb5ccf310fb6b0ed1
Erin Millard <contact@erin.id.au>
2023-01-23 08:35:09 +1000

    Remove brew autoupdate

--- setup/50-packages.bash
+++ setup/50-packages.bash
@@ -3,8 +3,3 @@ set -e
 echo "Installing packages..."
 
 brew bundle "--file=$HOME/Brewfile"
-
-echo "Setting up automatic Homebrew upgrades..."
-
-mkdir -p "$HOME/Library/LaunchAgents"
-brew autoupdate start

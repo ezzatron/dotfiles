093b19bb2638e74b9d1b5e4b3e5b09c930f139d3
Erin Millard <contact@erin.id.au>
2023-02-06 13:45:22 +1000

    Fix Grit config generation

--- setup-extension/20-grit.bash
+++ setup-extension/20-grit.bash
@@ -1,12 +1,13 @@
 set -e
 
-if [[ -f "$EXTENSION_DOTFILES_PATH/dotfiles/.config/grit.toml" ]]; then
+if [[ -f "$EXTENSION_DOTFILES_PATH/dotfiles/grit.sources" ]]; then
   echo "Configuring Grit for $EXTENSION_NAME..."
 
   echo "# Generated file - DO NOT EDIT" > "$HOME/.config/grit.toml"
   echo >> "$HOME/.config/grit.toml"
+  echo "[clone.sources]" >> "$HOME/.config/grit.toml"
 
-  for FILE in "$HOME/dotfiles/extensions/"*/dotfiles/.config/grit.toml; do
+  for FILE in "$HOME/dotfiles/extensions/"*/dotfiles/grit.sources; do
     echo "# From $FILE" >> "$HOME/.config/grit.toml"
     cat "$FILE" >> "$HOME/.config/grit.toml"
   done

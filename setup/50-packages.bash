5add3e8915265b5adc86714d19599d4ab2c5b16e
Erin Millard <contact@erin.id.au>
2023-06-30 20:54:51 +1000

    Add an alias from docker to podman

--- setup/50-packages.bash
+++ setup/50-packages.bash
@@ -3,3 +3,10 @@ set -e
 echo "Installing packages..."
 
 brew bundle "--file=$HOME/Brewfile"
+
+if [[ -e "$HOME/bin/docker" ]]; then
+  echo "Alias from podman to docker already exists."
+else
+  echo "Aliasing podman to docker..."
+  ln -s "$(which podman)" "$HOME/bin/docker"
+fi

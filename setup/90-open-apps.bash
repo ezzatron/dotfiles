8c2125242a7760d4fa1465100e7782d276de580f
Erin Millard <contact@erin.id.au>
2024-04-02 09:09:45 +1000

    Switch from Podman back to Docker

--- setup/90-open-apps.bash
+++ setup/90-open-apps.bash
@@ -4,7 +4,7 @@ echo "Opening apps..."
 
 open "/Applications/1Password.app"
 open "/Applications/Google Chrome.app"
-open "/Applications/Podman Desktop.app"
+open "/Applications/Docker.app"
 open "/Applications/Scroll Reverser.app"
 open "/Applications/Slack.app"
 open "/Applications/Visual Studio Code.app"

da0b507b25dedc94042f276cffecb7afba6d4e3c
Erin Millard <contact@erin.id.au>
2024-11-13 10:50:49 +1000

    Remove nvm

--- dotfiles/.zshenv
+++ dotfiles/.zshenv
@@ -86,9 +86,6 @@ if [[ $SHLVL = 1 ]]; then
   # .NET
   export DOTNET_ROOT="$(brew --prefix)/opt/dotnet/libexec"
 
-  # Node.js
-  export NVM_DIR="$HOME/.nvm"
-
   # include private env vars
   source "$HOME/.zshenv.private"
 
--- setup/30-directories.bash
+++ setup/30-directories.bash
@@ -3,6 +3,5 @@ set -e
 echo "Creating directories..."
 
 [ -e "$HOME/.z" ] || mkdir -p "$HOME/.z"
-[ -e "$HOME/.nvm" ] || mkdir -p "$HOME/.nvm"
 [ -e "$HOME/bin" ] || mkdir -p "$HOME/bin"
 [ -e "$HOME/go" ] || mkdir -p "$HOME/go"

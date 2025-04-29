3b3d4a81fe62a9ea4dc16a3248d8fe3cdcc7cdbd
Erin Millard <contact@erin.id.au>
2025-04-29 14:49:02 +1000

    Add AlDente

--- setup/90-open-apps.bash
+++ setup/90-open-apps.bash
@@ -3,9 +3,10 @@ set -e
 echo "Opening apps..."
 
 open "/Applications/1Password.app"
+open "/Applications/AlDente.app"
+open "/Applications/Docker.app"
 open "/Applications/Google Chrome.app"
 open "/Applications/iTerm.app"
-open "/Applications/Docker.app"
 open "/Applications/Scroll Reverser.app"
 open "/Applications/Slack.app"
 open "/Applications/Visual Studio Code.app"

309de46eebde02247a84f6f69fbc3bdace1afd01
Erin Millard <contact@erin.id.au>
2025-04-29 14:51:32 +1000

    Stop installing asdf tools globally

--- setup/51-asdf.bash
+++ setup/51-asdf.bash
@@ -4,11 +4,3 @@ echo "Installing asdf plugins..."
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add protoc https://github.com/paxosglobal/asdf-protoc.git
-
-echo "Installing asdf tools..."
-
-asdf install nodejs latest
-asdf set --home nodejs system
-
-asdf install protoc latest
-asdf set --home protoc system

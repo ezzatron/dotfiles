5772fcbadab82d1c68cc6263f9fc45578cf0c569
Erin Millard <contact@erin.id.au>
2025-08-17 21:10:15 +1000

    Reformat home.nix

--- home.nix
+++ home.nix
@@ -1,15 +1,20 @@
 { pkgs, ... }:
 {
   home = {
+    stateVersion = "25.11";
+
     sessionVariables = {
       PATH = "$HOME/bin:$PATH";
+    };
+  };
+
+  programs = {
+    home-manager = {
+      enable = true;
     };
   };
 
   imports = [
     ./home/iterm.nix
   ];
-
-  programs.home-manager.enable = true;
-  home.stateVersion = "25.11";
 }

55eed6182d6b97de925715147e4c5df9f82ce6ea
Erin Millard <contact@erin.id.au>
2025-08-18 14:24:07 +1000

    Attempt to set SSH_AUTH_SOCK for Grit

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,9 +1,15 @@
 { ... }:
 {
+  home = {
+    sessionVariables = {
+      SSH_AUTH_SOCK = "${user.home}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    };
+  };
+
   programs.ssh = {
     enable = true;
     extraConfig = ''
-      IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+      IdentityAgent "${user.home}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
     '';
   };
 }

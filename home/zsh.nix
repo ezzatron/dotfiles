5bce6933cbdc1b2731a120c45234a4afdc68fbaf
Erin Millard-Wright <contact@erin.id.au>
2025-08-18 16:15:18 +1000

    Add git wrapper function

--- home/zsh.nix
+++ home/zsh.nix
@@ -11,6 +11,30 @@
       enable = true;
     };
 
+    siteFunctions = {
+      git = ''
+        # override standard git commands with aliases
+
+        if [ "$1" = "show" ]; then
+          command git x-show "''${@:2}"
+        elif [ "$1" = "push" ]; then
+          # prevent use force push without lease
+          if [ "$ALLOW_FORCE" != "true" ]; then
+            for ARG in "''${@:2}"; do
+              if [ "$ARG" = "-f" ] || [ "$ARG" = "--force" ]; then
+                echo -e "\033[0;31mYou are trying to force push without lease. Use 'git push --force-with-lease' instead.\033[0m"
+                return 1
+              fi
+            done
+          fi
+
+          command git "$@"
+        else
+          command git "$@"
+        fi
+      '';
+    };
+
     syntaxHighlighting = {
       enable = true;
     };

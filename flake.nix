fd658d7df38fd124caed78e3d435e9abfec3a2f1
Erin Millard-Wright <contact@erin.id.au>
2025-08-19 10:58:42 +1000

    Add system defaults

--- flake.nix
+++ flake.nix
@@ -191,7 +191,189 @@
               stateVersion = 6;
 
               defaults = {
+                NSGlobalDomain = {
+                  # turn off font smoothing (better for Retina displays, but worse for non-Retina)
+                  AppleFontSmoothing = 0;
+
+                  # use dark mode always
+                  AppleInterfaceStyle = "Dark";
+
+                  # Set a blazingly fast keyboard repeat rate
+                  KeyRepeat = 2;
+                  InitialKeyRepeat = 25;
+
+                  # Disable janky animation when zooming windows (works intermittently)
+                  NSAutomaticWindowAnimationsEnabled = false;
+
+                  # Enable tap-to-click
+                  "com.apple.mouse.tapBehavior" = 1;
+
+                  # Enable volume change feedback beep
+                  "com.apple.sound.beep.feedback" = 1;
+
+                  # Disable auto correct and other substitutions
+                  NSAutomaticCapitalizationEnabled = false;
+                  NSAutomaticDashSubstitutionEnabled = false;
+                  NSAutomaticPeriodSubstitutionEnabled = false;
+                  NSAutomaticQuoteSubstitutionEnabled = false;
+                  NSAutomaticSpellingCorrectionEnabled = false;
+
+                  # use 24-hour time format
+                  AppleICUForce24HourTime = true;
+                  # TODO
+                  # AppleICUDateFormatStrings = {
+                  #   "1" = "y-MM-dd";
+                  #   "2" = "y-MM-dd";
+                  #   "3" = "y-MM-dd";
+                  #   "4" = "y-MM-dd";
+                  # };
+                };
+
+                SoftwareUpdate = {
+                  # Automatic updates
+                  AutomaticallyInstallMacOSUpdates = true;
+                  # TODO
+                  # AutomaticCheckEnabled = true;
+                  # AutomaticDownload = true;
+                  # ConfigDataInstall = true;
+                  # CriticalUpdateInstall = true;
+                };
+
+                dock = {
+                  # Disable the Dock by default
+                  autohide = true;
+                  autohide-delay = 1000.0;
+
+                  # Right-oriented Dock
+                  orientation = "right";
+
+                  # Show only open applications in the Dock
+                  static-only = true;
+
+                  # Wipe all (default) app icons from the Dock
+                  persistent-apps = [ ];
+
+                  # Show hidden dock items as translucent
+                  showhidden = true;
+
+                  # Set the dock icon size
+                  tilesize = 64;
+
+                  # Speed up Mission Control animations
+                  expose-animation-duration = 0.1;
+
+                  # Top-left hot corner turns on screen saver when holding the command key
+                  wvous-tl-corner = 5;
+                  # TODO
+                  # wvous-tl-modifier = 1048576;
+
+                  # Top-right hot corner tuns off screen when holding the command key
+                  wvous-tr-corner = 10;
+                  # TODO
+                  # wvous-tr-modifier = 1048576;
+
+                  # Bottom-right hot corner is disabled
+                  wvous-br-corner = 1;
+
+                  # Bottom-left hot corner is disabled
+                  wvous-bl-corner = 1;
+
+                  # Don’t show Dashboard as a Space
+                  dashboard-in-overlay = true;
+                };
+
+                trackpad = {
+                  # Enable tap-to-click
+                  Clicking = true;
+                };
+
+                universalaccess = {
+                  # Disable animation when switching screens or opening apps
+                  reduceMotion = true;
+                };
+
+                screencapture = {
+                  # Save screenshots to a dedicated directory
+                  location = "${user.home}/Screenshots";
+
+                  # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+                  type = "png";
+                };
+
+                finder = {
+                  # Allow Finder to be quit normally
+                  QuitMenuItem = true;
+
+                  # Show the Finder path bar
+                  ShowPathbar = true;
+
+                  # Show the Finder status bar
+                  ShowStatusBar = true;
+
+                  # Hide Finder recent tags
+                  # TODO
+                  # ShowRecentTags = false;
+
+                  # Open folders in new Finder windows instead of tabs
+                  # TODO
+                  # FinderSpawnTab = false;
+
+                  # Use list view in all Finder windows by default
+                  FXPreferredViewStyle = "Nlsv";
+
+                  # Keep directories at the top when sorting in Finder
+                  _FXSortFoldersFirst = true;
+
+                  # Use the home directory as the default location for new Finder windows
+                  NewWindowTarget = "Home";
+
+                  # Finder should search the current directory by default
+                  FXDefaultSearchScope = "SCcf";
+
+                  # Turn off automatic Desktop icons
+                  ShowExternalHardDrivesOnDesktop = false;
+                  ShowHardDrivesOnDesktop = false;
+                  ShowMountedServersOnDesktop = false;
+                  ShowRemovableMediaOnDesktop = false;
+
+                  # Automatically open a new Finder window when a volume is mounted
+                  # TODO
+                  # OpenWindowForNewRemovableDisk = true;
+                };
+
                 CustomUserPreferences = {
+                  ".GlobalPreferences" = {
+                    # Set programmer-friendly word break characters
+                    AppleTextBreakLocale = "en_US_POSIX";
+                  };
+
+                  "com.apple.menuextra.clock" = {
+                    # Set menu clock format
+                    DateFormat = "EEE MMM d h:mm a";
+                  };
+
+                  "com.apple.frameworks.diskimages" = {
+                    # Automatically open a new Finder window when a volume is mounted
+                    auto-open-ro-root = true;
+                    auto-open-rw-root = true;
+                  };
+
+                  "com.apple.dashboard" = {
+                    # Disable Dashboard
+                    mcx-disabled = true;
+                  };
+
+                  # TODO
+                  # "/Library/Preferences/com.apple.commerce.plist" = {
+                  #   # Automatic updates
+                  #   AutoUpdate = true;
+                  # };
+
+                  "com.apple.dt.Xcode" = {
+                    # Force Xcode to use the built-in version of Git for installing dependencies
+                    IDEPackageSupportUseBuiltinSCM = "YES";
+                  };
+
                   "com.googlecode.iterm2" = {
                     # Don’t display the annoying prompt when quitting iTerm
                     PromptOnQuit = false;
@@ -201,6 +383,21 @@
                     PrefsCustomFolder = "${user.home}/.config/iterm2";
                     LoadPrefsFromCustomFolder = true;
                   };
+
+                  "com.valvesoftware.steam.streamingclient" = {
+                    # Disable window closing shortcuts for Steam remote play
+                    NSUserKeyEquivalents = "{Close=\"\\U200b\";\"Quit steamstreamingclient\"=\"~\\Uf707\";}";
+                  };
+                };
+              };
+
+              activationScripts = {
+                postActivation = {
+                  enable = true;
+                  text = ''
+                    # Following line should allow us to avoid a logout/login cycle when changing settings
+                    sudo -u ${user.name} /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+                  '';
                 };
               };
             };

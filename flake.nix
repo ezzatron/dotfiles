11a38c660fc556af6145d4e6cc42639f32fa7a39
Erin Millard-Wright <contact@erin.id.au>
2025-09-27 20:44:29 +1000

    Move Homebrew packages to separate file

--- flake.nix
+++ flake.nix
@@ -81,6 +81,9 @@
         }
         (
           { config, ... }:
+          let
+            brew = import ./brew.nix;
+          in
           {
             homebrew = {
               enable = true;
@@ -89,48 +92,7 @@
               onActivation.upgrade = true;
               taps = builtins.attrNames config.nix-homebrew.taps;
 
-              brews = [
-                "cloudflare-wrangler" # For testing against Cloudflare Worker runtime. Nix package installation takes forever.
-                "jmalloc/grit/grit" # No Nix package.
-                "mas" # To avoid mas getting uninstalled every time.
-              ];
-
-              casks = [
-                "1password-cli"
-                "1password"
-                "aldente"
-                "calibre"
-                "chatgpt"
-                "discord"
-                "docker-desktop"
-                "figma"
-                "google-chrome"
-                "guitar-pro"
-                "iina"
-                "iterm2"
-                "keyboardcleantool"
-                "messenger"
-                "microsoft-edge"
-                "scroll-reverser"
-                "slack"
-                "steam"
-                "visual-studio-code"
-
-                {
-                  name = "firefox@developer-edition";
-                  args = {
-                    language = "en-GB";
-                  };
-                }
-              ];
-
-              masApps = {
-                "1Password for Safari" = 1569813296;
-                "MusicHarbor" = 1440405750;
-                "Pixelmator Pro" = 1289583905;
-                "TestFlight" = 899247664;
-                "Xcode" = 497799835;
-              };
+              inherit (brew) brews casks masApps;
             };
           }
         )

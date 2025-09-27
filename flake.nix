32a3ac1c005996e7a56643e82cb1c1b412da0835
Erin Millard-Wright <contact@erin.id.au>
2025-09-27 20:35:58 +1000

    Move packages to separate file

--- flake.nix
+++ flake.nix
@@ -154,34 +154,7 @@
         (
           { pkgs, ... }:
           {
-            environment.systemPackages = [
-              pkgs.adrs
-              pkgs.bun
-              pkgs.caddy
-              pkgs.clang-tools # To format .proto files
-              pkgs.colordiff
-              pkgs.deno
-              pkgs.dotnetCorePackages.dotnet_8.sdk
-              pkgs.evans
-              pkgs.gh
-              pkgs.git
-              (pkgs.writeShellScriptBin "gsed" "exec -a $0 ${pkgs.gnused}/bin/sed \"\$@\"")
-              (pkgs.google-cloud-sdk.withExtraComponents [
-                pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
-              ])
-              pkgs.jq
-              pkgs.kubectl
-              pkgs.kubectx
-              pkgs.kubernetes-helm
-              pkgs.nixfmt-rfc-style # To format .nix files
-              pkgs.nodejs_24
-              pkgs.powershell
-              pkgs.protobuf
-              pkgs.terraform
-              pkgs.unixtools.watch
-              pkgs.vale
-              pkgs.zsh-history-substring-search
-            ];
+            environment.systemPackages = import ./packages.nix { inherit pkgs; };
 
             fonts.packages = [
               pkgs.nerd-fonts.victor-mono

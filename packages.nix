4782ef2050972c81f43de7721a10444a497a5ed0
Erin Millard-Wright <contact@erin.id.au>
2025-11-28 16:52:04 +1000

    Add Go

--- packages.nix
+++ packages.nix
@@ -12,6 +12,7 @@
   pkgs.gh
   pkgs.git
   (pkgs.writeShellScriptBin "gsed" "exec -a $0 ${pkgs.gnused}/bin/sed \"\$@\"")
+  pkgs.go
   pkgs.go-task
   (pkgs.google-cloud-sdk.withExtraComponents [
     pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
